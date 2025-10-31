c5bd833f49381b92ffded22cdc43ea968a473b8c
Kenneth <kogimurio@gmail.com>
2025-10-31 19:05:44 +0300

    ADDED: Error handles

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,4 +1,7 @@
 'use client';
+ 
+
+import { useActionState } from 'react';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -9,18 +12,15 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
 
-export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
-}) {
+export default function EditInvoiceForm({ invoice, customers, }: { invoice: InvoiceForm; customers: CustomerField[]; }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const initialState: State = { message: null, errors: {}};
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
